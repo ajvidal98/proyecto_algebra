7db627809692544ecde86a56df049c96c63bc460
ajvidal98 <alvarovdl4@gmail.com>
2017-11-14 20:57:45 -0300

    ingresando figura rombo y mostrando su perimetro y area.

--- Desktop/proyecto_1/src/main/Main.java
+++ Desktop/proyecto_1/src/main/Main.java
@@ -19,9 +19,11 @@ public class Main {
         double perimetro_escaleno=0.0;
         double perimetro_cuadrado=0.0;
         double perimetro_rectangulo=0.0;
+        double perimetro_rombo=0.0;
         double baserectangulo=0.0;
         double alturarectangulo=0.0;
         double arearectangulo=0.0;
+        double area_rombo=0.0;
         double radio=0.0;
         double pi=3.1416;
         double areacirculo=0.0;
@@ -29,7 +31,9 @@ public class Main {
         double alturacuadrado=0.0;
         double areacuadrado=0.0;
         int opc;
+        int opcion;
         while(true){
+            System.out.println("                                        ");
             System.out.println("----------------------------------------");
             System.out.println("Sofwere de Algebra");
             System.out.println("----------------------------------------");
@@ -38,24 +42,31 @@ public class Main {
             System.out.println("2.- 4 Puntos.");
             System.out.println("3.-Salir.");
             System.out.println("----------------------------------------");
-            System.out.println("ingrese su opcion: ");
+            System.out.print("ingrese su opcion: ");
             opc = leer.nextInt();
             System.out.println("----------------------------------------");
             
             if(opc == 1){
-                
-                System.out.print("Ingrese las coordenadas de la base: ");
+                System.out.println("                                             ");
+                System.out.println("---------------------------------------------");
+                System.out.println("Ingrese las coordenadas de la base: ");
+                System.out.println("---------------------------------------------");
                 
                 crdA = leer.nextDouble();
                 
                 
-                
+                System.out.println("                                             ");
+                System.out.println("---------------------------------------------");
                 System.out.println("Ingrese las cordenadas de la altura: ");
+                System.out.println("---------------------------------------------");
                 
                 crdB = leer.nextDouble();
                 
               
+                System.out.println("                                             ");
+                System.out.println("---------------------------------------------");
                 System.out.println("Ingrese las cordenadas de un lado x1:");
+                System.out.println("---------------------------------------------");
                 
                 crdC = leer.nextDouble();
                 area =(crdA * crdB)/2;
@@ -166,54 +177,100 @@ public class Main {
                 
             }else {
                 if(opc == 2){
+                    System.out.println("                                         ");
+                    System.out.println("-----------------------------------------");
                     System.out.println("Ingrese las cordenadas de la base: ");
+                    System.out.println("-----------------------------------------");
                     
                     crdA = leer.nextDouble();
                     
                     
-                    
+                    System.out.println("                                         ");
+                    System.out.println("-----------------------------------------");
                     System.out.println("Ingrese las cordenadas de la altura: ");
-                    
+                    System.out.println("-----------------------------------------");
                     crdB = leer.nextDouble();
                     
                     
-                    
+                    System.out.println("                                         ");
+                    System.out.println("-----------------------------------------");
                     System.out.println("Ingrese las cordenadas de parte superior: ");
+                    System.out.println("-----------------------------------------");
                     
                     crdC = leer.nextDouble();
                     
                     
-                    
+                    System.out.println("                                         ");
+                    System.out.println("-----------------------------------------");
                     System.out.println("Ingrese las cordenadas de lado x1: ");
+                    System.out.println("-----------------------------------------");
                     
                     crdD = leer.nextDouble();
                     
                     perimetro_cuadrado = crdA + crdB + crdC + crdD;
                     if(crdA == crdB && crdA == crdC && crdA == crdD){
-                        areacuadrado =(crdA * crdB);
-                        System.out.println("---------------------------------------");
-                        System.out.println("la figura ingresada es un cuadrado.");
-                        System.out.println("---------------------------------------");
-                        System.out.println("                                       ");
-                        System.out.println("          * * * * * * * * * *");
-                        System.out.println("          * * * * * * * * * *");
-                        System.out.println("          * * * * * * * * * *");
-                        System.out.println("          * * * * * * * * * *");
-                        System.out.println("          * * * * * * * * * *");
-                        System.out.println("          * * * * * * * * * *");
-                        System.out.println("          * * * * * * * * * *");
-                        System.out.println("          * * * * * * * * * *");
-                        System.out.println("          * * * * * * * * * *");
-                        System.out.println("          * * * * * * * * * *");
-                        System.out.println("                                                 ");
-                        System.out.println("-------------------------------------------------");
-                        System.out.println("el area del cuadrado es: "+areacuadrado);
-                        System.out.println("-------------------------------------------------");
-                        System.out.println("el perimetro del cuadrado es: "+perimetro_cuadrado);
+                        System.out.println("                                                    ");
+                        System.out.println("------------------------------------------------------");
+                        System.out.println("las cordenadas ingresadas pueden ser de dos figuras.");
+                        System.out.println("------------------------------------------------------");
+                        System.out.println("1.-Cuadrado");
+                        System.out.println("2.-Rombo");
+                        System.out.println("--------------------");
+                        System.out.print("que opcion desea:");
+                        opcion = leer.nextInt();
+                        
+                        if(opcion == 1){
+                          areacuadrado =(crdA * crdB);
+                            System.out.println("---------------------------------------");
+                            System.out.println("la figura ingresada es un cuadrado.");
+                            System.out.println("---------------------------------------");
+                            System.out.println("                                       ");
+                            System.out.println("          * * * * * * * * * *");
+                            System.out.println("          * * * * * * * * * *");
+                            System.out.println("          * * * * * * * * * *");
+                            System.out.println("          * * * * * * * * * *");
+                            System.out.println("          * * * * * * * * * *");
+                            System.out.println("          * * * * * * * * * *");
+                            System.out.println("          * * * * * * * * * *");
+                            System.out.println("          * * * * * * * * * *");
+                            System.out.println("          * * * * * * * * * *");
+                            System.out.println("          * * * * * * * * * *");
+                            System.out.println("                                                 ");
+                            System.out.println("-------------------------------------------------");
+                            System.out.println("el area del cuadrado es: "+areacuadrado);
+                            System.out.println("-------------------------------------------------");
+                            System.out.println("el perimetro del cuadrado es: "+perimetro_cuadrado);  
+                        }else{
+                            area_rombo = crdA * crdC /2;
+                            perimetro_rombo = crdA *4;
+                            if(opcion == 2){
+                                System.out.println("---------------------------------------");
+                                System.out.println("la figura ingresada es un Rombo.");
+                                System.out.println("---------------------------------------");
+                                System.out.println("                                       ");
+                                System.out.println("                    *");
+                                System.out.println("                  * * *");
+                                System.out.println("                * * * * *");
+                                System.out.println("              * * * * * * *");
+                                System.out.println("            * * * * * * * * *");
+                                System.out.println("              * * * * * * *");
+                                System.out.println("                * * * * *");
+                                System.out.println("                  * * *");
+                                System.out.println("                    *");
+                                System.out.println("                                                 ");
+                                System.out.println("                                                 ");
+                                System.out.println("-------------------------------------------------");
+                                System.out.println("el area del rombo es: "+area_rombo);
+                                System.out.println("-------------------------------------------------");
+                                System.out.println("el perimetro del rombo es: "+perimetro_rombo);
+                            }
+                        }
+                        
                     }else{
                         perimetro_rectangulo = crdA + crdA + crdB + crdB;
+                        arearectangulo = crdA * crdB;
                         if(crdA == crdC){
-                            arearectangulo = crdA * crdB;
+                            
                             System.out.println("---------------------------------------");
                             System.out.println("la figura ingresada es un rectangulo.");
                             System.out.println("---------------------------------------");
